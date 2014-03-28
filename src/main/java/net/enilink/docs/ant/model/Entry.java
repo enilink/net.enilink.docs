5f8b972e72673d8546e3b713d482096aacfdc779
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2014-03-28 14:36:01 +0100

    Providing generators for documentation

---
+++ src/main/java/net/enilink/docs/ant/model/Entry.java
@@ -0,0 +1,15 @@
+package net.enilink.docs.ant.model;
+
+public class Entry {
+	public final String title;
+	public final String path;
+	
+	public Entry(String title, String path) {
+		if (path.startsWith("/")) {
+			path = path.substring(1);
+		}
+		this.title = title;
+		this.path = path;
+	}
+
+}
---
+++ src/main/java/net/enilink/docs/ant/model/Node.java
@@ -0,0 +1,27 @@
+package net.enilink.docs.ant.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+	public List<Node> children = new ArrayList<>();
+
+	public List<Entry> entries = new ArrayList<>();
+
+	public String title;
+
+	public Node(String title) {
+		this.title = title;
+	}
+
+	public void add(Node node) {
+		children.add(node);
+	}
+
+	@Override
+	public String toString() {
+		return title;
+	}
+
+}
